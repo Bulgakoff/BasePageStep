6449724d5c86f62ddc9742438199feb66fd83fb7
Alex <hlbu@yandex.ru>
2021-10-18 22:45:43 +0300

    first commit

---
+++ test_main_page.py
@@ -0,0 +1,15 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def go_to_login_page(browser):
+    login_link = browser.find_element_by_css_selector("#login_link")
+    print(f'============================= {login_link.text}')
+    login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
+    time.sleep(4)
